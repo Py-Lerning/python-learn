d99b456ff474382835ac3176aeeb4fd2ae3389a0
Harry Liu <lxy729358795@163.com>
2020-05-14 00:56:18 +0800

    如何用对象进行序列化

--- lxy/io/serialize.py
+++ lxy/io/serialize.py
@@ -9,6 +9,7 @@ def python_dump():
     a = {'name': 'dream', 'age': '18'}
     a = json.dumps(a)  # dumps 加载进去
 
+    # dumps()方法返回一个str，内容就是标准的JSON。
     print(isinstance(a, str))  # True
     with open('json_test.json', 'w') as f:
         f.write(a)
@@ -33,5 +34,42 @@ def python_load():
         print(a['name'])  # dream
         f.close()
 
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+class Car(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+def json2object(d):
+    className = d.pop('__class__', None)
+    if className:
+        print(globals())    # 返回全局变量的字典。
+        cls = globals()[className]
+        return cls(**d)
+    return d
+
+text = '''
+{
+  "__class__": "Student",
+  "name": "Bob",
+  "age": 12,
+  "score": 95
+}
+'''
+
+
+
 if __name__ == "__main__":
-    python_load()
+    # python_load()
+
+    s = json.loads(text, object_hook=json2object)
+
+    print(s.name)
+    print(s.age)
+    print(s.score)
